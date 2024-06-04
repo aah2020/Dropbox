c45a1430e06c8dc5bcb2384e60caa05280453761
Abdullah Al Hasib <abdullah.alhasib@kratosdefense.com>
2024-06-04 03:03:10 +0200

    Added integration test for a single file addition

--- test_integration/src/sync_test.cpp
+++ test_integration/src/sync_test.cpp
@@ -6,6 +6,8 @@
 #include <thread>
 #include <functional>
 #include <iostream>
+#include <fstream>
+#include "fileop.h"
 
 using namespace dropbox;
 
@@ -21,6 +23,11 @@ public:
 
     void SetUp()
     {
+        create_dir("./test_srcDir/");
+        create_dir("./test_dstDir/");
+        std::ofstream ofs_src;
+        ofs_src.open ("./test_srcDir/test.txt", std::ofstream::out | std::ofstream::app);
+        ofs_src.close();
     }
 
     void TearDown()
@@ -31,29 +38,46 @@ public:
 
     void server_process()
     {
-        DropboxServer* server = new DropboxServer("/home/kratos/test_dstDir/", "127.0.0.1", 8050, 10);
+        DropboxServer server("./test_dstDir/", "127.0.0.1", 8050, 10);
+        std::cerr << "Starting server" << std::endl;
+        server.start();
     }
 
     void client_process()
     {
-        DropboxClient* client = new DropboxClient("/home/kratos/test_srcDir/", "127.0.0.1", 8050, 10);
+        DropboxClient client("./test_srcDir/", "127.0.0.1", 8050, 10);
+        client.start();
     }
 
     ~SyncTest()
     {
         // cleanup any pending stuff, but no exceptions allowed
     }
+
+        std::thread t1;
+        std::thread t2;
 };
 
-TEST_F (SyncTest, ClientServerCommunication)
+TEST_F (SyncTest, SingleFile)
 {
-    int quit = false;
-    std::thread t1(std::bind(&SyncTest::server_process, this));
+    try
+    {
+        t1 = std::thread(std::bind(&SyncTest::server_process, this));
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     std::this_thread::sleep_for (std::chrono::seconds(1));
-    std::thread t2(std::bind(&SyncTest::client_process, this));
-    std::this_thread::sleep_for (std::chrono::seconds(10));
-    quit = true;
-    (void)quit;
+
+    try
+    {
+        t2 = std::thread(std::bind(&SyncTest::client_process, this));
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     t2.join();
     t1.join();
